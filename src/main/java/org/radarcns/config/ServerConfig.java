2a008eb1a710e8742cd84d0d6216560598e7e597
Joris Borgdorff <joris@thehyve.nl>
2017-03-14 09:48:37 +0100

    Added ServerConfig to commons

---
+++ src/main/java/org/radarcns/config/ServerConfig.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright 2017 Kings College London and The Hyve
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.radarcns.config;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.net.InetSocketAddress;
+import java.net.MalformedURLException;
+import java.net.Proxy;
+import java.net.Proxy.Type;
+import java.net.URL;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import javax.annotation.Nonnull;
+
+/**
+ * POJO representing a ServerConfig configuration
+ */
+public class ServerConfig {
+    private String host;
+    private int port = -1;
+    private String protocol;
+    @JsonProperty("proxy")
+    private String proxy;
+    @JsonProperty("proxy_port")
+    private int proxyPort = -1;
+
+    /** Get the path of the server as a string. This does not include proxy information. */
+    public String getPath() {
+        if (protocol != null) {
+            return protocol + "://" + host + ":" + port;
+        } else {
+            return host + ":" + port;
+        }
+    }
+
+    /** Get the paths of a list of servers, concatenated with commas. */
+    public static String getPaths(@Nonnull List<ServerConfig> configList) {
+        return configList.stream().map(ServerConfig::getPath).collect(Collectors.joining(","));
+    }
+
+    /**
+     * Get the server as a URL.
+     *
+     * @return URL to the server.
+     * @throws MalformedURLException if protocol is not set or the host name is invalid.
+     */
+    public URL getURL() throws MalformedURLException {
+        return new URL(protocol, host, port, "");
+    }
+
+    /**
+     * Get the HTTP proxy associated to given server
+     * @return http proxy if specified, or null if none is specified.
+     * @throws IllegalStateException if proxy is set but proxyPort is not or if the server protocol
+     *                               is not HTTP(s)
+     */
+    public Proxy getHttpProxy() {
+        if (proxy == null) {
+            return null;
+        } else if (proxyPort == -1) {
+            throw new IllegalStateException("proxy_port is not specified for server " + getPath()
+                    + " with proxy");
+        }
+        if (protocol != null
+                && !protocol.equalsIgnoreCase("http")
+                && !protocol.equalsIgnoreCase("https")) {
+            throw new IllegalStateException(
+                    "Server is not an HTTP(S) server, so it cannot use a HTTP proxy.");
+        }
+        return new Proxy(Type.HTTP, new InetSocketAddress(proxy, proxyPort));
+    }
+
+    /** Server host name or IP address. */
+    public String getHost() {
+        return host;
+    }
+
+    /** Set server host name or IP address. */
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    /** Server port. Defaults to -1. */
+    public int getPort() {
+        return port;
+    }
+
+    /** Set server port. */
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    /** Server protocol. */
+    public String getProtocol() {
+        return protocol;
+    }
+
+    /** Set server protocol. */
+    public void setProtocol(String protocol) {
+        this.protocol = protocol;
+    }
+
+    /** Proxy host name. Null if not set. */
+    public String getProxy() {
+        return proxy;
+    }
+
+    /** Set proxy host name. */
+    public void setProxy(String proxy) {
+        this.proxy = proxy;
+    }
+
+    /** Proxy port. Defaults to -1. */
+    public int getProxyPort() {
+        return proxyPort;
+    }
+
+    /** Set proxy port. */
+    public void setProxyPort(int proxyPort) {
+        this.proxyPort = proxyPort;
+    }
+
+    @Override
+    public String toString() {
+        return getPath();
+    }
+}
