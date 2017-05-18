69f9874ad849fd2d89a74fdf38d949c4b94778ab
Joris Borgdorff <joris@thehyve.nl>
2017-05-18 09:10:50 +0200

    MockDevice closes senders again

--- testing/src/main/java/org/radarcns/mock/MockDevice.java
+++ testing/src/main/java/org/radarcns/mock/MockDevice.java
@@ -75,20 +75,27 @@ public class MockDevice<K extends SpecificRecord> extends Thread {
             }
             Oscilloscope oscilloscope = new Oscilloscope(baseFrequency);
 
-            while (!stopping.get()) {
-                // The time keeping is regulated with beats, with baseFrequency beats per second.
-                int beat = oscilloscope.beat();
+            try {
+                while (!stopping.get()) {
+                    // The time keeping is regulated with beats, with baseFrequency beats per
+                    // second.
+                    int beat = oscilloscope.beat();
 
-                for (int i = 0; i < generators.size(); i++) {
-                    int frequency = generators.get(i).getConfig().getFrequency();
-                    if (frequency > 0 && beat % (baseFrequency / frequency) == 0) {
-                        Record<K, SpecificRecord> record = recordIterators.get(i).next();
-                        topicSenders.get(i).send(record.offset, record.key, record.value);
+                    for (int i = 0; i < generators.size(); i++) {
+                        int frequency = generators.get(i).getConfig().getFrequency();
+                        if (frequency > 0 && beat % (baseFrequency / frequency) == 0) {
+                            Record<K, SpecificRecord> record = recordIterators.get(i).next();
+                            topicSenders.get(i).send(record.offset, record.key, record.value);
+                        }
                     }
                 }
+            } catch (InterruptedException ex) {
+                // do nothing, just exit the loop
             }
-        } catch (InterruptedException ex) {
-            // do nothing, just exit the loop
+
+            for (KafkaTopicSender<K, SpecificRecord> topicSender : topicSenders) {
+                topicSender.close();
+            }
         } catch (IOException e) {
             synchronized (this) {
                 this.exception = e;
