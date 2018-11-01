ab39d02f346db5e0d4d07c20bbbddefd3f39f4c0
Joris Borgdorff <joris@thehyve.nl>
2018-11-01 13:29:24 +0100

    Fix Javadoc order

--- radar-commons/src/main/java/org/radarcns/producer/rest/RecordRequest.java
+++ radar-commons/src/main/java/org/radarcns/producer/rest/RecordRequest.java
@@ -43,8 +43,8 @@ public interface RecordRequest<K, V> {
     /**
      * Return the content of the record as a string. To avoid dual reading of data for RecordData
      * that does not store the results, prepare and reset may be called around this method.
-     * @return the content.
      * @param maxLength maximum returned length
+     * @return the content.
      * @throws IOException if the content cannot be written.
      */
     String content(int maxLength) throws IOException;
