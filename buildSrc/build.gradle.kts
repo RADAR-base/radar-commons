49921a8938127207429eb06e33e58f5ebd40e3b2
Joris Borgdorff <joris@thehyve.nl>
2023-03-22 13:20:59 +0100

    Remove unused dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,13 +4,4 @@ plugins {
 
 repositories {
     mavenCentral()
-    gradlePluginPortal()
-}
-
-dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.8.10")
-    implementation("com.github.ben-manes:gradle-versions-plugin:0.46.0")
-    implementation("io.github.gradle-nexus:publish-plugin:1.3.0")
-    implementation("org.jlleitschuh.gradle:ktlint-gradle:11.3.1")
 }
