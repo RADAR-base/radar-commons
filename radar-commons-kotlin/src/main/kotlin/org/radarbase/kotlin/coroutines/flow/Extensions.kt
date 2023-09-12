1c9731da85755a835ebbd85d9e4ee5aa15cd1721
Joris Borgdorff <joris@thehyve.nl>
2023-09-12 14:28:44 +0200

    Added javadoc to List<Flow<T>>.zipAll

--- radar-commons-kotlin/src/main/kotlin/org/radarbase/kotlin/coroutines/flow/Extensions.kt
+++ radar-commons-kotlin/src/main/kotlin/org/radarbase/kotlin/coroutines/flow/Extensions.kt
@@ -5,15 +5,21 @@ import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.zip
 
+/**
+ * Convert a list of flows to one flow with a list of values. The list contains the latest value of
+ * each respective flow, in the same order as the original flows.
+ */
 fun <T> List<Flow<T>>.zipAll(): Flow<List<T>> = when (val numberOfFlows = size) {
     0 -> flowOf(listOf())
     1 -> first().map { listOf(it) }
     2 -> first().zip(last()) { a, b -> listOf(a, b) }
-    else -> subList(0, numberOfFlows - 1)
-        .zipAll()
+    else -> subList(0, numberOfFlows - 1).zipAll()
         .zip(last()) { rest, last -> rest + last }
 }
 
+/**
+ * Alias of [List.zipAll].
+ */
 fun <T> zipAll(
     vararg flows: Flow<T>,
 ): Flow<List<T>> = flows.toList().zipAll()
