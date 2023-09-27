7295b0c3008e318c1311461eba567d71484d3940
Joris Borgdorff <joris@thehyve.nl>
2023-09-27 16:36:23 +0200

    Bump avro

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -13,7 +13,7 @@ object Versions {
     const val slf4j = "2.0.9"
     const val confluent = "7.5.0"
     const val kafka = "7.5.0-ce"
-    const val avro = "1.11.2"
+    const val avro = "1.11.3"
     const val jackson = "2.15.2"
     const val okhttp = "4.11.0"
     const val junit = "5.10.0"
