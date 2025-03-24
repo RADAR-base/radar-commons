e83fcb196799f513e086d1a9ef906105af6adfef
pvannierop <pim@thehyve.nl>
2025-03-24 12:17:27 +0100

    Set project version to 1.2.1
    Was incorrect in previous release.

--- radar-commons-gradle/build.gradle.kts
+++ radar-commons-gradle/build.gradle.kts
@@ -181,7 +181,7 @@ tasks.withType<PublishToMavenRepository> {
 // They should be copied from the Versions.kt file directly to maintain consistency.
 @Suppress("ConstPropertyName", "MemberVisibilityCanBePrivate")
 object Versions {
-    const val project = "1.1.3"
+    const val project = "1.2.1"
 
     object Plugins {
         const val licenseReport = "2.5"
